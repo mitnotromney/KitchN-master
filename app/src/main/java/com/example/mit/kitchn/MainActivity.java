649ada1c787e677acc0c1bfcf397f5345ab0ea96
mitnotromney <timharrisco@gmail.com>
2020-02-12 20:42:56 -0600

    Fixed clunky ui issues

--- app/src/main/java/com/example/mit/kitchn/MainActivity.java
+++ app/src/main/java/com/example/mit/kitchn/MainActivity.java
@@ -7,6 +7,8 @@ import android.media.Image;
 import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
